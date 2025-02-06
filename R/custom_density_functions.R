e854b91897e981de56053ab3892aebe70940b4e0
andrewwilson201 <andrewm.wilson@gmail.com>
2025-02-06 13:25:33 +0000

    density changes

--- R/custom_density_functions.R
+++ R/custom_density_functions.R
@@ -1,27 +1,3 @@
-#' Pareto density function wrapper for fitdistrplus
-#'
-#' @param x Numeric vector.
-#' @param shape Shape parameter.
-#' @param scale Scale parameter.
-#' @param log Logical; if TRUE, probabilities p are given as log(p).
-#' @return Density evaluated at x.
-#' @export
-dpareto <- function(x, shape, scale, log = FALSE) {
-  actuar::dpareto(x, shape = shape, scale = scale, log = log)
-}
-
-#' Inverse Gaussian density function wrapper for fitdistrplus
-#'
-#' @param x Numeric vector.
-#' @param mean Mean parameter.
-#' @param shape Shape parameter.
-#' @param log Logical; if TRUE, probabilities p are given as log(p).
-#' @return Density evaluated at x.
-#' @export
-dinvgauss <- function(x, mean, shape, log = FALSE) {
-  actuar::dinvgauss(x, mean = mean, shape = shape, log = log)
-}
-
 #' Inverse Gaussian density function wrapper for fitdistrplus
 #'
 #' @param x Numeric vector.
@@ -43,7 +19,7 @@ custom_invgauss <- list(
   r = function(n, mean, shape) {
     actuar::rinvgauss(n, mean = mean, shape = shape)
   },
-  name = "invgauss"
+  name = ".invgauss"
 )
 
 #' Pareto density function wrapper for fitdistrplus
@@ -67,5 +43,5 @@ custom_pareto <- list(
   r = function(n, shape, scale) {
     actuar::rpareto(n, shape = shape, scale = scale)
   },
-  name = "pareto"
+  name = ".pareto"
 )
